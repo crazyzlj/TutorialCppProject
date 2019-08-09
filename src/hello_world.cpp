df16916090d67295d27e3e430a97b4e49e5c3550
crazyzlj <crazyzlj@gmail.com>
2019-08-09 17:27:58 +0800

    add hello_world executable for the demo of CMake

---
+++ src/hello_world.cpp
@@ -0,0 +1,16 @@
+/*!
+ * Simple C++ program to display "Hello World" 
+ */
+// Header file for input output functions 
+#include<iostream>  
+
+// Using the cout and endl function in the std namespace
+using std::cout;
+using std::endl;
+  
+// main function where the execution of program begins 
+int main() { 
+    // prints hello world 
+    cout << "Hello World" << endl;
+    return 0;
+}
